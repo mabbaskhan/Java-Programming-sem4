89005bf8b5e1755c1d9f445ef8c474be13d9b7ae
M ABBs <mabbaskhan797@gmail.com>
2021-03-09 23:12:00 +0500

    cheak it and correct it.

---
+++ assignment#1.java
@@ -0,0 +1,12 @@
+class GratvityCalculator {
+    public static void main(string[] args){
+        double fallingPosition;
+        double gravity = -9.8;
+        double initialVelocity = 0.0;
+        double fallingTime = 10.0;
+        double initialPosition = 0.0;
+        fallingPosition = (0.5 * gravity * (fallingTime * fallingTime) + (initialVilocity * fallingTime) + initialPosition);
+        System.out.println(x);
+    }
+
+}
